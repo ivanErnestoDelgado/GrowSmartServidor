c4d92cd1e3729bb432b8456b627229dab81b5451
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2024-10-18 21:22:06 -0700

    modelo añadido y arvchivo de serializer creado para la app de usuarios

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import User
+from django.db import models
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100, blank=True, null=True)  # Ubicación geográfica del usuario
+    phone_number = models.CharField(max_length=15, blank=True, null=True)  # Número de teléfono opcional
+    bio = models.TextField(blank=True, null=True)  # Información adicional del usuario
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
