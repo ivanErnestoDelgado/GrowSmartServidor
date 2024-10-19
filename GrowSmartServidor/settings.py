c7e0d646e12b19c7bf6d5bdfdbaa594b547c2393
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2024-10-19 13:34:01 -0700

    app de macetas y usuarios enlazada con la app principal

--- GrowSmartServidor/settings.py
+++ GrowSmartServidor/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'plants',
-    'rest_framework'
+    'rest_framework',
+    'users',
+    'smartpots'
 ]
 
 MIDDLEWARE = [
