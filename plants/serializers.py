146a8ee548f4544f3957e1257c05bfbd0c415c94
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2024-10-18 21:20:57 -0700

    Serializer añadido

--- plants/serializers.py
+++ plants/serializers.py
@@ -1,1 +1,21 @@
 from rest_framework import serializers
+from .models import Plant
+
+#Serializador del modelo de Plants
+class PlantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Plant
+        fields=(
+            'id',
+            'name',
+            'scientific_name',
+            'description',
+            'ideal_temperature',
+            'ideal_humidity',
+            'sunlight_needs',
+            'watering_frequency',
+            'created_at',
+            'updated_at'
+        )
+        read_only_fields=('created_at',)
+
