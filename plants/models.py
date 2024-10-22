00a18d4d7209d3c99916c1b965031d828efa7321
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2024-10-22 09:43:10 -0700

    modificaciones en los modelos de la app de plantas

--- plants/models.py
+++ plants/models.py
@@ -4,13 +4,18 @@ class Plant(models.Model):
     name = models.CharField(max_length=100)  # Nombre casual de la planta
     scientific_name = models.CharField(max_length=100, blank=True, null=True)  # Nombre científico
     description = models.TextField(blank=True, null=True)  # Descripción de la planta
-    ideal_temperature = models.DecimalField(max_digits=5, decimal_places=2)  # Temperatura ideal
-    ideal_humidity = models.DecimalField(max_digits=5, decimal_places=2)  # Nivel de humedad ideal (%)
+    maximun_temperature = models.DecimalField(max_digits=5, decimal_places=2)  # Temperatura maxima
+    minimun_temperature = models.DecimalField(max_digits=5, decimal_places=2)  # Temperatura minima
+    maximun_humidity = models.DecimalField(max_digits=5, decimal_places=2)  # Nivel de humedad maximo
+    minumun_humidity = models.DecimalField(max_digits=5, decimal_places=2)  # Nivel de humedad minimo
+    maximun_ligth_level=models.PositiveIntegerField() # Nivel de luz maximo
+    minimun_ligth_level=models.PositiveIntegerField()   # Nivel de humedad minimo
     sunlight_needs = models.CharField(max_length=100)  #  Tipo de exposicion a la luz solar (ej. "Directa", "Sombra")
     watering_frequency = models.PositiveIntegerField()  # Frecuencia de riego en días
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    plant_cares=models.TextField()
     def __str__(self):
         return self.name
 
+#ACTUALIZAR EL SERIALIZER DESPUES
