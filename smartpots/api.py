e7537c15e3ffe513906f0bff6cef33e4c9ca7d19
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2024-10-25 12:47:45 -0700

    Endpoints de eliminacion y edicion de macetas funcional

--- smartpots/api.py
+++ smartpots/api.py
@@ -3,6 +3,8 @@ from rest_framework.permissions import IsAuthenticated
 from .models import SmartPot
 from .serializers import SmartPotCreateSerializer,SmartPotSerializer
 from users.models import UserProfile
+from rest_framework.exceptions import PermissionDenied
+
 class SmartPotCreateView(generics.CreateAPIView):
     serializer_class = SmartPotCreateSerializer
     permission_classes = [IsAuthenticated]
@@ -17,3 +19,15 @@ class UserSmartPotListView(generics.ListAPIView):
 
     def get_queryset(self):
         return SmartPot.objects.filter(user_profile=UserProfile.objects.get(user=self.request.user))
+
+class SmartPotDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SmartPot.objects.all()
+    serializer_class = SmartPotCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        # Obtenemos la maceta por ID y verificamos que pertenezca al usuario autenticado
+        obj = super().get_object()
+        if obj.user_profile != UserProfile.objects.get(user=self.request.user):
+            raise PermissionDenied("No tienes permiso para modificar esta maceta.")
+        return obj
--- smartpots/urls.py
+++ smartpots/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .api import SmartPotCreateView, UserSmartPotListView
+from .api import SmartPotCreateView, UserSmartPotListView,SmartPotDetailView
 
 urlpatterns = [
     path('api/smartpots/create/', SmartPotCreateView.as_view(), name='create_smartpot'),
     path('api/smartpots/', UserSmartPotListView.as_view(), name='user_smartpots'),
+    path('api/smartpots/<int:pk>/', SmartPotDetailView.as_view(), name='smartpot_detail'),
 ]
