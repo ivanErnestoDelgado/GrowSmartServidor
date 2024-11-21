52c324236cba4a277df75ccb5ca38ab190ed87de
ivanErnestoDelgado <dino_fan2002@live.com.mx>
2024-11-21 16:04:38 -0700

    modificaciones al modelo de plants, se le añadieron nuevos metodos para evaluar un dato del senson en base a los limites de la planta y se añadio un enum que se utiliza en los mismos metodos

--- plants/models.py
+++ plants/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from enum import Enum
 # Create your models here.
 class Plant(models.Model):
+    class dataStatusFromSensor(Enum):
+        LOWER=0
+        NORMAL=1
+        HIGHER=2
+
     name = models.CharField(max_length=100)  # Nombre casual de la planta
     maximun_temperature = models.DecimalField(max_digits=5, decimal_places=2)  # Temperatura maxima
     minimun_temperature = models.DecimalField(max_digits=5, decimal_places=2,)  # Temperatura minima
@@ -12,7 +18,23 @@ class Plant(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.name
+    
+    
+    def evaluate_light_data(self,sensor_light_data):
+        return self.obtain_data_status(sensor_light_data,self.minimun_ligth_level,self.maximun_ligth_level)
 
+    def evaluate_temperature_data(self, sensor_temperature_data):
+        return self.obtain_data_status(sensor_temperature_data,self.minimun_temperature,self.maximun_temperature)
+    
+    def evaluate_humidity_data(self, sensor_humidity_data):
+        return self.obtain_data_status(sensor_humidity_data,self.minumun_humidity,self.maximun_humidity)
+    
+    def obtain_data_status(self,sensor_data,minimun,maximun):
+        if(sensor_data<minimun):
+            return self.dataStatusFromSensor.LOWER
+        if(sensor_data>maximun):
+            return self.dataStatusFromSensor.HIGHER
+        return self.dataStatusFromSensor.NORMAL
 
 class PlantRecomendation(models.Model):
     class RecommendationType(models.TextChoices):
