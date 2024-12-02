1d63a47978664ac0c68412f8e30e73d009200da5
FemboyIRL <gatoelluis@gmail.com>
2024-12-01 23:07:45 -0600

    10  plants added

--- plants/api.py
+++ plants/api.py
@@ -11,7 +11,14 @@ class PlantListView(APIView):
         plants = Plant.objects.all()
         serializer = PlantSerializer(plants, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
+    
+    def post(self, request):
+        # Crear una nueva planta
+        serializer = PlantSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()  # Guardar la planta
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class PlantDetailWithRecommendationsView(generics.RetrieveAPIView):
     queryset = Plant.objects.all()
